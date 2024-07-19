6552b2aec1878668792ff7769bd89d2353f248a5
lbilla512 <lakshmi512.billa@gmail.com>
2024-07-19 16:07:09 +0530

    all divisors

--- demo/src/main/java/com/example/demo/AllDivisorsOfANumber.java
+++ demo/src/main/java/com/example/demo/AllDivisorsOfANumber.java
@@ -3,9 +3,18 @@ package com.example.demo;
 public class AllDivisorsOfANumber {
 
     static void allDivisorsOfANumber(int input){
-        System.out.println(input);
+        for(int i =1; i*i <= input; i++){
+           if(input % i == 0){
+               System.out.println(i);
+               if(i != input /i) {
+                   System.out.println(input / i);
+               }
+           }
+        }
     }
     public static void main(String[] args){
-        allDivisorsOfANumber(13);
+        allDivisorsOfANumber(15);
+        allDivisorsOfANumber(100);
+        allDivisorsOfANumber(7);
     }
 }
