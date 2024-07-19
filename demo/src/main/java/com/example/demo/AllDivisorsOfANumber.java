b988761b91a092bb0310ee6b7b0fe17f4f6750f5
billl <lakshmi.billa@in.pega.com>
2024-07-19 15:36:56 +0530

    test

---
+++ demo/src/main/java/com/example/demo/AllDivisorsOfANumber.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class AllDivisorsOfANumber {
+
+    static void allDivisorsOfANumber(int input){
+        System.out.println(input);
+    }
+    public static void main(String[] args){
+        allDivisorsOfANumber(13);
+    }
+}
